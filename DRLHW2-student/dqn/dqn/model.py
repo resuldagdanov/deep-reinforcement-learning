0971bc66fa9e733be5255d012e3590537966d546
resul_dagdanov <resul.dagdanov@eatron.com>
2022-04-23 18:33:07 +0300

    dqn model loss implementation

--- DRLHW2-student/dqn/dqn/model.py
+++ DRLHW2-student/dqn/dqn/model.py
@@ -6,12 +6,13 @@ from dqn.base_dqn import BaseDQN
 
 
 class DQN(BaseDQN):
-    """ Deep Q Network agent.
+    """
+        Deep Q Network agent.
 
-    Args:
-        valuenet (torch.nn.Module): Neural network to estimate Q values
-        nact (int):  Number of actions (or outputs)
-        buffer (UniformBuffer): Uniform Replay Buffer
+        Args:
+            valuenet (torch.nn.Module): Neural network to estimate Q values
+            nact (int):  Number of actions (or outputs)
+            buffer (UniformBuffer): Uniform Replay Buffer
     """
 
     def __init__(self, valuenet: torch.nn.Module, nact: int, buffer: UniformBuffer):
@@ -19,29 +20,52 @@ class DQN(BaseDQN):
         self.buffer = buffer
 
     def push_transition(self, transition: UniformBuffer.Transition) -> None:
-        """ Push transition to replay buffer.
-
-        Args:
-            transition (UniformBuffer.Transition): One step transition
         """
+            Push transition to replay buffer.
+
+            Args:
+                transition (UniformBuffer.Transition): One step transition
+        """
+        
         self.buffer.push(transition)
 
     def loss(self, batch: UniformBuffer.Transition, gamma: float) -> torch.Tensor:
-        """ TD loss that uses the target network to estimate target values
-
-        Args:
-            batch (UniformBuffer.Transition): Batch of transitions
-            gamma (float): Discount factor
-
-        Returns:
-            torch.Tensor: TD loss
         """
-        raise NotImplementedError
-        #  /$$$$$$$$ /$$$$$$ /$$       /$$
-        # | $$_____/|_  $$_/| $$      | $$
-        # | $$        | $$  | $$      | $$
-        # | $$$$$     | $$  | $$      | $$
-        # | $$__/     | $$  | $$      | $$
-        # | $$        | $$  | $$      | $$
-        # | $$       /$$$$$$| $$$$$$$$| $$$$$$$$
-        # |__/      |______/|________/|________/
+            TD loss that uses the target network to estimate target values
+
+            Args:
+                batch (UniformBuffer.Transition): Batch of transitions
+                gamma (float): Discount factor
+
+            Returns:
+                torch.Tensor: TD loss
+        """
+
+        # getting an available device (cuda is prefered)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        # the size of the batch
+        batch_size = batch.state.shape[0]
+
+        # decode transition batch tuples and represent in torch tensor format
+        state = torch.Tensor(batch.state).to(device)
+        action = torch.Tensor(batch.action).to(device)
+        next_state = torch.Tensor(batch.next_state).to(device)
+        reward = torch.Tensor(batch.reward).to(device).view(batch_size)
+        terminal = torch.Tensor(batch.terminal).float().to(device)
+
+        # target network (comes from BaseDQN class) is used to find the values of the next-states
+        value_next_state = self.targetnet(next_state).to(device)
+
+        # compute Bellman expectation
+        expected_value_state = (value_next_state * gamma) + reward
+
+        # get action state value from current value network
+        value_state = self.valuenet(state).gather(1, action[:, 0].view(-1,1).long()).view(-1)
+
+        # calculate loss between expected value and current value of the state
+        loss_function = torch.nn.MSELoss(reduction='mean')
+        loss = loss_function(expected_value_state, value_state)
+
+        return loss
+        
